24d16d38c33dc0b509721eabb2a1f91c81a063f4
Timothy Wesley <timothywesley@mac.mynetworksettings.com>
2025-07-04 23:53:47 -0400

    removed the NUMBER env variable

--- backend/utils/db.py
+++ backend/utils/db.py
@@ -8,7 +8,7 @@ testing = True
 collection_name = "test" if testing else "production"
 
 url = os.environ["MONGO_URL"]
-work_number = os.environ["NUMBER"]
+
 client = AsyncMongoClient(url)
 
 db = client["work-phone"]
