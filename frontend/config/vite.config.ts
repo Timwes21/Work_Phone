7c9d7d340e26645dc3b01d9f8fecb391db6e6255
Timothy Wesley <timothywesley@mac.mynetworksettings.com>
2025-07-06 04:16:45 -0400

    still trying to see the port 7

--- frontend/config/vite.config.ts
+++ frontend/config/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from "dotenv";
+dotenv.config();
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+
+console.log("Vite preview starting on port:", process.env?.PORT);
 
 
