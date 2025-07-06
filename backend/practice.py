dd583b8514d524cbc73cf7c79d4394ba30c32fb4
Timothy Wesley <timothywesley@mac.mynetworksettings.com>
2025-07-06 03:07:53 -0400

    still trying to see the port

--- backend/practice.py
+++ backend/practice.py
@@ -6,23 +6,5 @@ from langchain_core.documents import Document
 from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
+test = "ggg"
 
-class Example:
-    calls = []
-    def __init__(self):
-        self.logs = []
-
-    def append_log(self, thing_to_append):
-        self.logs.append(thing_to_append)
-
-    def append_calls(self, thing_to_append):
-        self.calls.append(thing_to_append)
-
-    def print_calls(self):
-        print(self.calls)
-
-example1 = Example()
-example2 = Example()
-
-example1.append_calls(1)
-example2.print_calls()
